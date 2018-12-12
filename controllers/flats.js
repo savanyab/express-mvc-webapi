54d253f25f0a6b7e3cd66d450aa4d2e2090399b4
savanyab <savanyab@gmail.com>
2018-12-12 16:09:01 +0100

    update and destroy

--- controllers/flats.js
+++ controllers/flats.js
@@ -3,10 +3,18 @@ const flats = express();
 const models = require('../models');
 
 // index
-flats.get('/', (req,res) => {
+flats.get('/', (req, res) => {
     models.Flat.findAll()
         .then(flats => {
             res.json(flats);
+        });
+});
+
+// create
+flats.post('/', (req, res) => {
+    models.Flat.create(req.body)
+        .then(flat => {
+            res.json(flat);
         });
 });
 
@@ -18,13 +26,23 @@ flats.get('/:id', (req, res) => {
         });
 });
 
-// create
-flats.post('/', (req, res) => {
-    models.Flat.create(req.body)
+
+// update
+flats.put('/:id', (req, res) => {
+    models.Flat.update(req.body, { where: { id: req.params.id } })
         .then(flat => {
             res.json(flat);
         });
 });
+
+// destroy
+flats.delete('/:id', (req, res) => {
+    models.Flat.destroy({ where: {id: req.params.id}})
+        .then(flat => {
+            res.json(flat);
+    });
+});
+
 
 
 
