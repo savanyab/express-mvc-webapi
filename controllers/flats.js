8fa60c12dfab39f60b94394de845661822369c4e
savanyab <savanyab@gmail.com>
2018-12-12 15:38:12 +0100

    index

--- controllers/flats.js
+++ controllers/flats.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const flats = express();
+const models = require('../models');
+
+// index
+flats.get('/', (req,res) => {
+    models.Flat.findAll()
+        .then(flats => {
+            res.json(flats);
+        });
+});
+
 
 
 module.exports = flats;
